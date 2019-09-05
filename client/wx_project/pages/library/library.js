b2e658619494573d3e6f41b33890bc512567bacc
ao <1147608791@qq.com>
2019-09-05 15:49:51 +0800

    书架

--- client/wx_project/pages/library/library.js
+++ client/wx_project/pages/library/library.js
@@ -5,62 +5,82 @@ Page({
    * 页面的初始数据
    */
   data: {
+    selectNum:0,
+    selected:true,
 
+    types: [{
+        name: "杂志",
+        id: 0
+      },
+      {
+        name: "号外",
+        id: 1
+      }
+    ]
+  },
+
+  changeType(item) {
+   /*console.log(this.data.selectNum);
+    console.log(item.index);*/
+    console.log(item);
+    this.setData({
+      selectNum: item.currentTarget.dataset.index
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {
+    console.log(this.data.selected)
+  },  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
