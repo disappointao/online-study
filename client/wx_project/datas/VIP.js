fbcc56bc6adffc6c4dd14e5402ddad847febde09
King <244414054@qq.com>
2019-09-06 12:25:30 +0800

    添加了点击事件

--- client/wx_project/datas/VIP.js
+++ client/wx_project/datas/VIP.js
@@ -1,9 +1,11 @@
 let VIP = [{
   name: "年度会员",
-  introduce: "365天畅读线上全部内容（含往期和《号外》）"
+  introduce: "365天畅读线上全部内容（含往期和《号外》）",
+  checked:false
 }, {
   name: "季度会员",
-  introduce: "阅读90天内出品的《Vista看天下》杂志"
+  introduce: "阅读90天内出品的《Vista看天下》杂志",
+  checked: false
 }]
 let info = [{
     info: ".年度：成功购买全年产品，可阅读自付费之日起365日内出品的《Visata看天下》杂志，同时"
--- client/wx_project/pages/mySubscribe/mySubscribe.js
+++ client/wx_project/pages/mySubscribe/mySubscribe.js
@@ -6,67 +6,81 @@ Page({
    * 页面的初始数据
    */
   data: {
-    data:[],
-    info:[]
+    data: [],
+    info: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       datas: datas.VIP,
-      info:datas.info
+      info: datas.info
     })
 
+  },
+  //点击函数
+  checked: function(res) {
+    console.log(datas.VIP)
+    // let data = datas;
+    for (let i = 0 ; i< datas.VIP.length;i++) {
+      console.log(datas.VIP.checked);
+      datas.VIP[i].checked = false;
+    }
+    let position = res.currentTarget.dataset.index;
+    datas.VIP[position].checked = true;
+    this.setData({
+      datas: datas.VIP
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
