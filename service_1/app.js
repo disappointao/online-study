0b9e5191eddf526211fbd74e0356d6068a9cde82
ao <1147608791@qq.com>
2019-09-07 23:10:46 +0800

    入口

--- service_1/app.js
+++ service_1/app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const router=require('./router');
 const path=require('path');
+
 let app=express();
 app.use('/public/',express.static(path.join(__dirname,'./public/')));
 app.use(router);
